8ff5cec924146ad890a594c3a8f8c38e9459a7b7
Jonathan de Souza Oliveira <jonathans@ciandt.com>
2018-11-12 23:57:47 -0200

    test first class domain

--- src/main/java/com/jonathan/cursomc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jonathan/cursomc/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,30 @@
 package com.jonathan.cursomc.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jonathan.cursomc.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-      return "rest funcionando";
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "informática");
+        Categoria cat2 = new Categoria(2, "escritório");
+
+        List<Categoria> list = new ArrayList<>();
+
+        list.add(cat1);
+        list.add(cat2);
+
+
+        return list;
   }
 }
